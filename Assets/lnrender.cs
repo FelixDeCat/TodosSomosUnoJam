285c50c313eb8174ca0563ec641c202590aa3fcb
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-12 17:14:10 -0300

    visual wp placeholder

---
+++ Assets/lnrender.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lnrender : MonoBehaviour
+{
+    public Transform l1;
+    public Transform l2;
+
+    public LineRenderer line;
+
+    private void Update()
+    {
+        line.SetPosition(0, l1.position);
+        line.SetPosition(1, l2.position);
+    }
+}
