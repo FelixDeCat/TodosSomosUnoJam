6ae0a23d8cc40be90065019871316b1e5b7c5b21
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-12 17:38:09 -0300

    bullet te hace daño

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Bullet : Enemy
 {
-    public override void OnTriggerEnter2D(Collider2D collision)
-    {
-        base.OnTriggerEnter2D(collision);
+    //public override void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    base.OnTriggerEnter2D(collision);
 
-        if (collision.gameObject.layer == 11)
-        {
-            GameObject.Destroy(gameObject);
-        }
-    }
+    //    if (collision.gameObject.layer == 11)
+    //    {
+    //        GameObject.Destroy(gameObject);
+    //    }
+    //}
 
-    public override void OnCollisionEnter2D(Collision2D collision)
-    {
-        base.OnCollisionEnter2D(collision);
+    //public override void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    base.OnCollisionEnter2D(collision);
 
-        GameObject.Destroy(gameObject);
-    }
+    //    GameObject.Destroy(gameObject);
+    //}
 }
