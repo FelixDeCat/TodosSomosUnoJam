e9959d943f7b16caf14a242d550d3f73503a8024
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-13 03:21:36 -0300

    Sound Data base Example

--- Assets/SoundDataBase.cs
+++ Assets/SoundDataBase.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class SoundDataBase : LoadComponent
 {
+    public static SoundDataBase instance;
     [SerializeField] AudioClip auEx;
+    [SerializeField] AudioClip cell_divide;
 
+    private void Awake() => instance = this;
     protected override IEnumerator LoadMe()
     {
         if (auEx) AudioManager.instance.GetSoundPool(auEx.name, AudioManager.AudioGroups.GAME_FX, auEx);
+        if (cell_divide) AudioManager.instance.GetSoundPool(cell_divide.name, AudioManager.AudioGroups.GAME_FX, cell_divide);
         yield return null;
     }
 
     public override void OnStartGame()
     {
 
+        //SoundDataBase.instance.PlayAuEx();
     }
 
-    public void PLayAuEx()
-    {
-        AudioManager.instance.PlaySound(auEx.name);
-    }
+    public void PlayAuEx() => AudioManager.instance.PlaySound(auEx.name);
+    public void PlayCellDivide() => AudioManager.instance.PlaySound(cell_divide.name);
 
-    
+
 }
